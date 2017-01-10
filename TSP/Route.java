ccbfbc6d9b433ad06c15466d3e78d09b247f1f3b
achen29 <achen28@stuy.edu>
2017-01-10 05:35:32 -0500

    Add files via upload

---
+++ TSP/Route.java
@@ -0,0 +1,17 @@
+public class Route {
+public int amount = 3;
+private City route[] = new City[amount];
+
+public Route(){
+	for(int i = 0 ;  i < getRoute().length ; i ++){
+		getRoute()[i] = new City();
+	}
+}
+public City[] getRoute(){
+	return route;
+}
+public void setRoute(City r[]){
+	this.route = r;
+}
+
+}
